ba2f215b83348fdfb250d0bd34847337ff4dfaed
Christian <christian.siegrist@open-circle.ch>
2025-10-10 10:18:44 +0200

    added event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,15 @@
 class Event < ApplicationRecord
+  belongs_to :creator, class_name: "User", foreign_key: :creator_id, inverse_of: :created_events
+  has_many :invites, inverse_of: :event, dependent: :destroy
+
+  #jointable for guests
+  has_many :event_guests, through: :invites, source: :event_guests, dependent: :destroy
+  # guests of the event
+  has_many :guests, through: :event_guests, source: :user  
+
+  # validations
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :date, presence: true
 end
--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,2 +1,10 @@
 class Invite < ApplicationRecord
+  belongs_to :event
+  belongs_to :user
+
+  has_many :event_guests, dependent: :destroy
+
+  # validations
+  validates :event_id, presence: true
+  validates :user_id, presence: true
 end
