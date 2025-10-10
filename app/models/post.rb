6195eaebe1dd5a42c76dfd13e5837db2b0704f63
Christian <christian.siegrist@open-circle.ch>
2025-10-10 10:07:30 +0200

    setup user, post and comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :root_comments, -> { where(parent_id: nil).order(created_at: :asc) },
+           class_name: "Comment"
+  validates :title, presence: true
+  validates :body, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # event creator relationship
+  has_many :created_events, class_name: "Event", foreign_key: :creator_id, inverse_of: :creator, dependent: :destroy
+  has_many :invites, through: :events
+
+  # jointable for guests
+  has_many :event_guests, inverse_of: :user, dependent: :destroy
+
+  # is guest of a event
+  has_many :upcomming_events, through: :event_guests, soure: :event
+
+  # posts and comments relationship
+  has_many :posts, inverse_of: :user, dependent: :destroy
+  has_many :comments, inverse_of: :user, dependent: :destroy
+
+  # validations
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
 end
